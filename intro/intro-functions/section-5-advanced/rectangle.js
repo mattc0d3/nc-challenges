2093d22f3cd59bccacfb38f14c016833f0f1bb2a
Matt <matthew.sheehan94@gmail.com>
2023-09-20 12:42:43 +0100

    complete section 5 intro functions rectangle challenge

--- intro/intro-functions/section-5-advanced/rectangle.js
+++ intro/intro-functions/section-5-advanced/rectangle.js
@@ -12,6 +12,19 @@ For example, makeRectangle(3,5) should print a 3 by 5 rectangle of *, as shown b
 Try to complete this task without using for loops!
 */
 
-function makeRectangle() {}
+function makeRectangle(m, n) {
+    let row = 0
+    let column = 0
+    while (column < n) {
+        while (row < m) {
+            process.stdout.write("* ")
+            row++
+        }
+        process.stdout.write("\n")
+        row = 0
+        column++
+    }
+}
 
 // invoke the function below to test it
+makeRectangle(3, 5)
