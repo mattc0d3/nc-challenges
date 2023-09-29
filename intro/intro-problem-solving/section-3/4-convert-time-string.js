729fcf1f7f5b66026d2116cdf027d13b3a25a645
Matt <matthew.sheehan94@gmail.com>
2023-09-29 11:01:34 +0100

    complete section 3 challenge 4 with refactored solution

--- intro/intro-problem-solving/section-3/4-convert-time-string.js
+++ intro/intro-problem-solving/section-3/4-convert-time-string.js
@@ -6,6 +6,21 @@ This function should take a string representing a time with hours and minutes se
 Some of the times are written in the 24-hour clock format
 This function should return the time written in the 12-hour clock format
   */
+  const regex = /^\d{2}(?=:)/
+  return string.replace(regex, (hour) => {
+    if (hour > 12) return ((hour < 22 ? "0" : "") + (parseInt(hour) - 12))
+    else if (hour == 0) return parseInt(hour) + 12
+    else return hour
+  })
+
+  const splitNums = string.split(":")
+  let hour
+  if (splitNums[0] !== undefined) {
+    hour = parseInt(splitNums[0])
+    if (hour > 12) hour -= 12
+    else if (hour === 0) hour += 12
+  }
+  return (String(hour).length === 1 ? "0" : "") + hour + ":" + splitNums[1]
 }
 
 console.log("convertTimeString()");
@@ -17,11 +32,11 @@ runTest(
   }
 );
 
-skipTest("converts an afternoon time to the 12 hour format", function () {
+runTest("converts an afternoon time to the 12 hour format", function () {
   check(convertTimeString("16:07")).isEqualTo("04:07");
 });
 
-skipTest(
+runTest(
   "converts times in the hour after midnight to the 12 hour format",
   function () {
     check(convertTimeString("00:56")).isEqualTo("12:56");
